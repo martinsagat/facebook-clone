b4698d45feb6410deeff9797d99ac214a4a0cb13
Martin Sagat <martin.sagat@outlook.com.au>
2021-04-02 00:16:33 +1100

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import router from './router.js';
+import App from './App.vue';
+
+require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+
+    components: {
+        App
+    },
+
+    router,
+});
