68fc2adeb3587121faf16f440cdb53ac51c92b0e
Martin Sagat <martin.sagat@outlook.com.au>
2021-04-03 23:17:33 +1100

    readme file

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="w-1/3 bg-white p-4 border-r border-gray-400">
-        <h2 class="font-bold text-2xl tracking-tight">Home</h2>
-        
-       
-        
+        <h2 class="font-bold text-2xl tracking-tight">Home</h2> 
     </div>
 </template>
 
